65f8cf8276a2c57da731391b456dccd320fdeb86
Guillehfg <132950209+Guillehfg@users.noreply.github.com>
2023-11-05 19:10:09 -0300

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+define('URL', '/viauy/');
+
+//define('URL', 'http://localhost/php_code_/proyectoCoderwise-main/proyectoCoderwise-main/');
+
+//conexion a la base de datos
+define('HOST', 'localhost');
+define('PORT_DB', '3306');
+define('DB', 'CoderWise');
+define('USER', 'root');
+define('PASSWORD', 'root');
+//define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');
